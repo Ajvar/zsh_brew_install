408de3d339ad242a8444336f96a542dc58bb7a56
Ajvar <cueille@protonmail.com>
2020-04-01 15:07:45 +0200

    First

---
+++ brew_zsh_install.sh
@@ -0,0 +1,2 @@
+curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && echo 'export PATH=$HOME/.brew/bin:$PATH' >> $HOME/.zshrc && source $HOME/.zshrc && brew update
