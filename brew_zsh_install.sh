627354bed97be63bf100847db954a3821d57de78
Joscelyn CUEILLE <jcueille@e3r7p3.42.fr>
2020-04-02 21:06:24 +0200

    zsh

--- brew_zsh_install.sh
+++ brew_zsh_install.sh
@@ -1,3 +1,4 @@
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh install.sh
 rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && export PATH=$HOME/.brew/bin:$PATH && brew update && echo "export PATH=$HOME/.brew/bin:$PATH" >> ~/.zshrc
 brew install nasm
