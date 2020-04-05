bc454f17410fdcd34bd4a762d4fd38b1a5c35d46
Joscelyn CUEILLE <jcueille@e3r2p9.42.fr>
2020-04-05 21:07:23 +0200

    removed ft_printf

--- brew_zsh_install.sh
+++ brew_zsh_install.sh
@@ -1,4 +1,3 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && export PATH=$HOME/.brew/bin:$PATH && brew update && echo "export PATH=$HOME/.brew/bin:$PATH" >> ~/.zshrc
 brew install nasm
-gcl git@vogsphere-v2.42.fr:vogsphere/intra-uuid-01c3b481-827e-4264-9ee3-d942aa505ce1-3119816
